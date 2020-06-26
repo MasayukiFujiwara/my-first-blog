cd9489d5a6b3acd105b550def29ad14685016e9b
masfujiwara1978 <masfujiwara1978@gmail.com>
2020-06-26 10:08:56 +0900

    django_blog 8 css type

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+
+STATICFILES_DIRS = (
+    "/home/fujiblogt/fujiblogt.pythonanywhere.com/mysite/static",
+)
